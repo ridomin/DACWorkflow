03f69b5decb4bdccaafcf57cb7fed34288d5455d
ridomin <ridomin@live.com>
2017-05-30 17:45:21 -0700

    CRC in COM and C#

--- MyDesktopApp/Form1.Designer.cs
+++ MyDesktopApp/Form1.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CRC";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- MyDesktopApp/Form1.cs
+++ MyDesktopApp/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,40 @@ namespace MyDesktopApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dr = openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 3)
+            {
+                RidoCRC.cBinaryFileStream file = new RidoCRC.cBinaryFileStream();
+                file.File = textBox1.Text;
+                RidoCRC.cCRC32 crc = new RidoCRC.cCRC32();
+                var res = crc.GetFileCrc32(file);
+                label1.Text = res.ToString();
+
+                DamienG.Security.Cryptography.Crc32 crc32 = new DamienG.Security.Cryptography.Crc32();
+                var f = File.OpenRead(textBox1.Text);
+                var res2 = crc32.ComputeHash(f);
+                label1.Text += " " + GetBigEndianUInt32(res2);
+            }
+        }
+
+        protected static UInt32 GetBigEndianUInt32(byte[] bytes)
+        {
+            if (bytes.Length != 4)
+                throw new ArgumentOutOfRangeException("bytes", "Must be 4 bytes in length");
+
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+            
+            return BitConverter.ToUInt32(bytes, 0);
         }
     }
 }
